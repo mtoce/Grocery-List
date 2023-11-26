0e87a9236a783783d561a49b0e022197ac797417
unknown <mptoce@gmail.com>
2023-11-26 11:14:03 -0500

    Added stateful functionality

--- src/Content.js
+++ src/Content.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Content = () => {
+    const [name, setName] = useState('Mike')
+    const [count, setCount] = useState(0)
     const handleNameChange = () => {
         const names = ['Bob', 'Kevin', 'Mike']
         const int = Math.floor(Math.random() * names.length)
-        return names[int];
-      }
+        setName(names[int]);
+    }
+
+    const handleClick = () => {
+        setCount(count + 1)
+        console.log(`Count ${count}`)
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`)
+    }
+
   return (
     <main>
-        <p>
-            Hello {handleNameChange()}!
+        <p onDoubleClick={handleClick}>
+            Hello {name}!
         </p>
+        <button onClick={handleNameChange}>
+            Change Name
+        </button>
+        <button onClick={handleClick}>
+            Click it!
+        </button>
+        <button onClick={handleClick2}>
+            Click it
+        </button>
     </main>
   )
 }
