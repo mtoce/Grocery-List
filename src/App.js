236dfd54dcf98f3b82a697ba1fa207e24e58df99
unknown <mptoce@gmail.com>
2023-12-05 14:46:07 -0500

    CRUD operations fully functional for updating items

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './Footer';
 import AddItem from './AddItem';
 import SearchItem from './SearchItem';
 import { useState, useEffect } from 'react';
+import apiRequest from './apiRequest';
 
 function App() {
 
@@ -36,17 +37,42 @@ function App() {
     // [] means useEffect will only operate at load time. Whatever is inside the brackets determines when useEffect will run. If we put our items array in, it would run every time the items array changed. (Which we don't want in this case since we're using useEffect to load out initial array. That could cause an infinite loop.)
   }, [])
 
-  const addItem = (item) => {
+  const addItem = async (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem]
     setItems(listItems);
+
+    const postOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(myNewItem)
+    }
+
+    // apiRequest returns an error. That will be the result. If the result is not null then we set the FetchError equal to that result.
+    const result = await apiRequest(API_URL, postOptions)
+    if (result) setFetchError(result)
   }
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     // Recreate the list of items from default state by checking if an item is checked by the user. Swap the checked status if it is checked and return the same item if it is not checked.
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item)
     setItems(listItems);
+
+    const myItem = listItems.filter((item) => item.id === id)
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ checked: myItem[0].checked})
+    }
+
+    const reqUrl = `${API_URL}/${id}`
+    const result = await apiRequest(reqUrl, updateOptions)
+    if (result) setFetchError(result)
   }
 
   const handleSubmit = (e) => {
@@ -56,17 +82,15 @@ function App() {
     setNewItem('');
   }
 
-  // const handleSearch = (e) => {
-  //   e.preventDefault();
-  //   if (!e.target.value) return;
-  //   const listItems = items.filter((item) => item.item.contains(e.target.value))
-  //   setItems(listItems);
-  // }
-
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     // console.log(id)
     const listItems = items.filter((item) => item.id !== id)
     setItems(listItems);
+
+    const deleteOptions = { method: 'DELETE' }
+    const reqUrl = `${API_URL}/${id}`
+    const result = await apiRequest(reqUrl, deleteOptions)
+    if (result) setFetchError(result)
   }
 
   return (
